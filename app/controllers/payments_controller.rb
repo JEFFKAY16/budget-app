81069b84c9dafb3e42209aed7f332324d5acb33e
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-06-01 02:08:48 +0200

    Fix linter errors on views

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,3 @@
 class PaymentsController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class PaymentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get payments_index_url
     assert_response :success
   end
