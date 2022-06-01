6821132f8f2ff420c613eb245405235800843766
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-06-01 18:21:57 +0200

    Image upload works fine

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,6 +26,6 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :icon)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :category_payments
   has_many :payments, through: :category_payments
+  has_one_attached :icon
 
   def recent_payments
     payments.order('id ASC')
