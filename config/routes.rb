a9139e4484a12ae8a0094815a81c0878ec24d2ca
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-06-01 17:34:47 +0200

    Payments successfully created in respective category

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'categories#index'
 
-  resources :categories, only: [:index, :show] do
-    resources :payments, only: [:index]
+  resources :categories, only: [:index, :show, :new, :create] do
+    resources :payments, only: [:index, :new, :create]
   end
 
 end
