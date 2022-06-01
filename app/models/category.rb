da12eaa58173cce1c058e2acf9a1307de70b4a39
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-06-01 03:10:05 +0200

    Fix linters from dynamic rendering

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :category_payments
   has_many :payments, through: :category_payments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-
   has_many :categories, foreign_key: 'author_id', class_name: 'Category'
   has_many :payments, foreign_key: 'author_id', class_name: 'Payment'
-
 end
