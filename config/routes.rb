1f87e68a8719398ea181821a993af627aec4a4d8
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-06-02 14:39:25 +0200

    Install devise & style devise forms success complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  root 'categories#index'
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'splash#index', as: :unauthenticated_root
+    end
+  end
 
   resources :categories, only: [:index, :show, :new, :create] do
     resources :payments, only: [:index, :new, :create]
